1eb65be5be469da585af087b617db5aa0a68a8b5
Kevin RRivera <krivera1015@gmail.com>
2018-11-08 23:25:11 -0500

    Containers and Components Folder added, Homepage Component Created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import FrontPage from "./containers/HomePage"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hey Im Kevin!
+        <FrontPage/>
       </div>
     );
   }
