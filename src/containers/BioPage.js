160918840c2b11fed005c73b3d1f14eb40623275
Kevin RRivera <krivera1015@gmail.com>
2018-11-16 06:45:34 -0500

    Adding grid now

--- src/containers/BioPage.js
+++ src/containers/BioPage.js
@@ -4,7 +4,7 @@ import BioCard from "../components/BioCard"
 class BioPage extends Component {
     render () {
         return (
-            <div>
+            <div className="Bio-page">
                 <BioCard/>
             </div>
         )
