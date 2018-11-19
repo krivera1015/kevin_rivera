1b7772607783de0f52a07f8e6572dcd8cdb35f0f
Kevin RRivera <krivera1015@gmail.com>
2018-11-19 17:25:25 -0500

    Finished the fun fact data, also building the front of card

--- src/components/FunFactWall.js
+++ src/components/FunFactWall.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import FunFactFrontCard from './FunFactFrontCard';
 import FunFactBackCard from './FunFactBackCard';
+import funFacts from '../funFactData'
 
 class FunFactWall extends Component {
+
+    state = {
+        funFacts: {}
+    }
+
+    componentDidMount() {
+        this.setState({funFacts})
+    }
+
     render () {
+        console.log(this.state.funFacts)
         return (
             <div>
-                <FunFactFrontCard/>
-                <FunFactBackCard/>
+                <FunFactFrontCard funFacts={this.state.funFacts}/>
+                <FunFactBackCard funFacts={this.state.funFacts}/>
             </div>
         )
     }
