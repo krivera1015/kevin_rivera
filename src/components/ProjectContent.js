304f3e1173a803519478a8ef957548ce8a29d963
Kevin RRivera <krivera1015@gmail.com>
2018-11-21 13:49:14 -0500

    Add my grids

--- src/components/ProjectContent.js
+++ src/components/ProjectContent.js
@@ -8,12 +8,12 @@ class ProjectContent extends Component {
                 <div className="Portfolio-header">
                     Recent Projects
                 </div>
-                <div className="Project-card-1">
+                <div className="Project-card">
                         <Card color='red' image={"https://res.cloudinary.com/dav4yqqvv/image/upload/v1542762114/Screen_Shot_2018-11-20_at_8.01.00_PM.png"} />
-                <div className="Project-card-2">
+                <div className="Project-card">
                         <Card color='orange' image={"https://res.cloudinary.com/dav4yqqvv/image/upload/v1542762417/Screen_Shot_2018-11-20_at_8.04.20_PM.png"} />
                 </div>
-                <div className="Project-card-3">
+                <div className="Project-card">
                         <Card color='yellow' image={"https://res.cloudinary.com/dav4yqqvv/image/upload/v1542762450/Screen_Shot_2018-11-20_at_8.07.17_PM.png"} />
                 </div>
                 </div>
