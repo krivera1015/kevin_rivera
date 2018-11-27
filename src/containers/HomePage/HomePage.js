cc3a6584e60594e05f8f2af933d95c748ec2f5af
Kevin RRivera <krivera1015@gmail.com>
2018-11-27 14:38:44 -0500

    Homepage 75% done

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
-//import '../css/HomePage.css';
+import './HomePage.css';
 
 class FrontPage extends Component {
     render () {
         return (
-            <div>
-                <div className="Home-page">
-                    <h1 className="Content">Who Is Kevin Rivera?</h1>
-                </div>
+            <div className="Home-page">
+                <h1 className="Content">Who Is Kevin Rivera?</h1>
             </div>
         )
     }
